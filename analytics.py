0e244ac52256db76ea6c09bdcf6db1266a340bab
ageekymonk <ramzthecoder@gmail.com>
2020-04-20 14:52:55 +1000

    Adding viewid and viewname in all the labels.

--- analytics.py
+++ analytics.py
@@ -8,8 +8,9 @@ from prometheus_client.core import REGISTRY, GaugeMetricFamily
 
 
 class GoogleAnalyticsCollector():
-    def __init__(self, viewid, credfile, scope):
+    def __init__(self, viewid, viewname, credfile, scope):
         self.viewid = viewid
+        self.viewname = viewname
         self.credfile = credfile
         self.scope = scope
 
@@ -22,21 +23,9 @@ class GoogleAnalyticsCollector():
             dimensions = query['dimensions']
 
             report_response = self.get_report(analytics, metrics, dimensions, date_range)
-            gauges = self.get_gauges(report_response)
+            gauges = self.get_gauges(self.viewid, self.viewname, report_response)
             for metric in gauges:
                 yield gauges[metric]
-
-        # response = self.get_user_report(analytics)
-        # user_gauges = self.get_gauges(response)
-
-        # for metric in user_gauges:
-        #     yield user_gauges[metric]
-
-        # response = self.get_session_report(analytics)
-        # session_gauges = self.get_gauges(response)
-
-        # for metric in session_gauges:
-        #     yield session_gauges[metric]
 
     def initialize_analyticsreporting(self):
         """Initializes an Analytics Reporting API V4 service object.
@@ -66,55 +55,31 @@ class GoogleAnalyticsCollector():
             }
         ).execute()
 
-    def get_user_report(self, analytics):
-        return analytics.reports().batchGet(
-            body={
-              'reportRequests': [
-                {
-                  'viewId': str(self.viewid),
-                  'dateRanges': [{'startDate': 'today', 'endDate': 'today'}],
-                  'metrics': [{'expression': 'ga:users'}],
-                  'dimensions': [{'name': 'ga:country'}, {'name': 'ga:userType'}]
-                }]
-            }
-        ).execute()
-
-    def get_session_report(self, analytics):
-        return analytics.reports().batchGet(
-            body={
-              'reportRequests': [
-                {
-                  'viewId': str(self.viewid),
-                  'dateRanges': [{'startDate': 'today', 'endDate': 'today'}],
-                  'metrics': [{'expression': 'ga:sessions'}, {'expression': 'ga:bounces'}],
-                  'dimensions': [{'name': 'ga:sessionDurationBucket'}]
-                }]
-            }
-        ).execute()
-
-    def get_gauges(self, response):
+    def get_gauges(self, viewid, viewname, response):
         _gauges = {}
         for report in response.get('reports', []):
             columnHeader = report.get('columnHeader', {})
             dimensionHeaders = columnHeader.get('dimensions', [])
             metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
 
+            labels = ['viewid', 'viewname'] + [elem[3:] for elem in dimensionHeaders]
             for metricHeader in metricHeaders:
-                _gauges[metricHeader.get('name')] = GaugeMetricFamily(metricHeader.get('name')[3:], 'Description of gauge', value=None, labels=[elem[3:] for elem in dimensionHeaders])
+                _gauges[metricHeader.get('name')] = GaugeMetricFamily(metricHeader.get('name')[3:], 'Description of gauge', value=None, labels=labels)
 
             for row in report.get('data', {}).get('rows', []):
                 dimensions = row.get('dimensions', [])
                 dateRangeValues = row.get('metrics', [])
 
+                labelvalues = [str(viewid), viewname] + dimensions
                 for i, values in enumerate(dateRangeValues):
                     for metricHeader, value in zip(metricHeaders, values.get('values')):
-                        _gauges[metricHeader.get('name')].add_metric(dimensions, value=value)
+                        _gauges[metricHeader.get('name')].add_metric(labelvalues, value=value)
         return _gauges
 
 
 def setup():
     for site in settings.SITES:
-        REGISTRY.register(GoogleAnalyticsCollector(site['viewid'], site['credsfile'], settings.SCOPES))
+        REGISTRY.register(GoogleAnalyticsCollector(site['viewid'], site['name'], site['credsfile'], settings.SCOPES))
         start_http_server(8000)
 
 
